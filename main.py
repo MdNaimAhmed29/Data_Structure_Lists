3186b9f8bff4c2fb1ed500b598211ae2e7e5eea3
Naim ahmed <naimahmed.vinacts@gmail.com>
2025-04-26 22:00:15 +0600

    update

--- main.py
+++ main.py
@@ -1,1 +1,48 @@
+"""
+-list[]
+    -mutable
+    -ordered
+    -most used
+    -allow duplicates
+    -heterogeneous
+    -dynamic size
+    -append()
+    -insert()
+    -extend()
+    -remove()
+    -pop()
+    -clear()
+    -sort()
+    -reverse()
+    -index()
+    -count()
+    -loop
+    -len()
+    -slicing
+"""
 
+#list
+
+fruits = ["apple", "banana", "cherry", "orange"]
+fruits2 = ["apple", "banana", "cherry", "orange"]
+fruits.pop()
+fruits.sort()
+print(fruits)
+fruits.reverse()
+print(fruits)
+fruits.extend(fruits2)
+print(fruits)
+fruits.remove("cherry")
+fruits.append("Mango")
+fruits.insert(0, "cherry")
+
+
+print(fruits)
+print(fruits[1:4])
+print(fruits.pop())
+print(fruits.index("cherry"))
+print(fruits.count("banana"))
+print(len(fruits))
+
+for fruit in fruits:
+    print(fruit)
